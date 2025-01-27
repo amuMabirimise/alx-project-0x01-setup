1639b5392861725f371ffbd59e6ecbc334a26cfb
amuMabirimise <awmabirimise@gmail.com>
2025-01-27 16:41:00 +0200

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,5 +1,17 @@
+import Header from "@/components/layout/Header";
+
 const Posts: React.FC = () => {
-  return <div className="p-4">Posts Page</div>;
+  return (
+    <div>
+      <Header />
+      <main className="p-8">
+        <h1 className="text-4xl font-bold">Posts Page</h1>
+        <p className="mt-4">
+          This is the Posts page. Explore all the posts here!
+        </p>
+      </main>
+    </div>
+  );
 };
 
 export default Posts;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,5 +1,17 @@
+import Header from "@/components/layout/Header";
+
 const Users: React.FC = () => {
-  return <div className="p-4">Users Page</div>;
+  return (
+    <div>
+      <Header />
+      <main className="p-8">
+        <h1 className="text-4xl font-bold">Users Page</h1>
+        <p className="mt-4">
+          This is the Users page. Find all user details here!
+        </p>
+      </main>
+    </div>
+  );
 };
 
 export default Users;
